3a482fc232d50e75c9eaf81d53beb06e6e223929
Artem <145000853+terehoff1985@users.noreply.github.com>
2024-03-13 13:53:14 +0700

    Update DivisionOperation.java

--- DivisionOperation.java
+++ DivisionOperation.java
@@ -1,9 +1,8 @@
 
-// Реализация операции деления
 public class DivisionOperation implements Operation {
     @Override
     public ComplexNumber calculate(ComplexNumber a, ComplexNumber b) {
         return b;
-        // реализация деления комплексных чисел
+
     }
 }
